acb177d6ec4b6a8b94041811a052d4906bc972d6
OzgeBagci <egzobagci@gmail.com>
2024-10-03 00:05:41 +0200

    Added photos

--- src/App.js
+++ src/App.js
@@ -13,7 +13,33 @@ function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Øzge Bagci</small>
+          <small>
+            This app was built by{" "}
+            <a
+              href="https://portfolio-project-ozge-bagci.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Øzge Bagci
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a
+              href="https://github.com/OzgeBagci/dictionary-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://dictionary-reactproject.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Netlify
+            </a>
+            .
+          </small>
         </footer>
       </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
-
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function search() {
     //documentation:https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey =
+      "EoOpSaBnmFKmHqWzKUNJkFKXosmNcy9KxWaZl0G7mp8NX6a5dgSTMrQp";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
+
   function handleSubmit(event) {
     event.preventDefault();
     search();
@@ -39,6 +50,7 @@ export default function Dictionary() {
         <div className="hint">E.g. Sunset, philosophy, wine... </div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
